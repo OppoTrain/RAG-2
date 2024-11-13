2b55104f8312ad041a3bdd83d8445f929c8c1802
unknown <saifalaa.099@gmail.com>
2024-11-14 00:27:27 +0200

    adding the docker files

--- frontend/chatbot_interface.py
+++ frontend/chatbot_interface.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import httpx
 
-# FastAPI endpoint URL (replace with your actual URL if deployed)
-API_URL = "http://127.0.0.1:8000/summarize"
+API_URL = "http://fastapi:8000/summarize"
 
 st.title("RAG Model Chatbot")
 st.write("Enter a query, and our RAG model will generate a summary for you.")
